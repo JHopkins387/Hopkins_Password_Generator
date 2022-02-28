07d1a1a2bf2680b08261eaa233f239eeb53ca873
Jarrett Hopkins <jarrettphopkins@Gmail.com>
2022-02-27 23:43:44 -0500

    Did some tiding up and minor fixes in code.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Global Values
 
-let upperCaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let lowerCaps = 'abcdefghijklmnopqrstubwxyz';
+let upperCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCaps = "abcdefghijklmnopqrstubwxyz";
 let specialCharacter = "!<>{}&^%$#@^&*()/:;";
-let numbers = '0123456789';
-let allCharacter = '';
-let length = '';
+let numbers = "0123456789";
+let allCharacter = "";
+let length = "";
 
 
 // Get references to the #generate element
@@ -18,60 +18,62 @@ function writePassword() {
   );
   //Checks to make sure a number is put in
   if (Number.isNaN(length)) {
-    alert('password length must be provided as a number');
+    alert('A password length must be provided as a number to continue.');
     return null;
   }
   //Makes sure that is not a number less than 8
   if (length < 8) {
-    alert('The password is too short. It must be at least 8 characters');
+    alert('The password is too short. It must be at least 8 characters.');
     return null;
   }
-  //Makes sure that the number is no more than 129
+  //Makes sure that the number is no more than 128
   if (length > 128) {
-    alert('The password is too long, it must be 128 or less characters');
+    alert('The password is too long, it must be 128 or less characters'.);
     return null;
   }
   //Stores if specific types of characters are allowd to be used
-  var addSpecialCharacter = confirm(
+  var addUppercase = confirm(
 
-    'Please click the Ok button to allow the use of Special characters.'
-  );
-
-  var addNumbers = confirm(
-
-    'Please click the Ok button to allow the use of numbers.'
+    'Please click the Ok button to allow the use of upper case leters.'
   );
 
   var addLowercase = confirm(
 
     'Please click the Ok button to allow the use of lower case letters.'
   );
+  var addNumbers = confirm(
 
-  var addUppercase = confirm(
-
-    'Please click the Ok button to allow the use of upper case leters.'
+    'Please click the Ok button to allow the use of numbers.'
   );
 
+  var addSpecialCharacter = confirm(
+
+    'Please click the Ok button to allow the use of Special characters.'
+  );
+
+
+  if (addUppercase) {
+    allCharacter += upperCaps;
+  }
 
   if (addLowercase) {
     allCharacter += lowerCaps;
   }
-  if (addUppercase) {
-    allCharacter += upperCaps;
-  }
-  if (addNumbers) {
-    allCharacter += numbers;
-  }
   if (addSpecialCharacter) {
     allCharacter += specialCharacter
   }
+
+  if (addNumbers) {
+    allCharacter += numbers;
+  }
+
   if (!allCharacter) {
-    alert('At least one character type must be use for this to work.');
+    alert("At least one character type must be use for this to work. Please choose at least one.");
     writePassword();
   }
 
   //randomization part
-  let newPassword = '';
+  let newPassword = "";
   for (let i = 0; i < length; i++) {
     newPassword += allCharacter.charAt(Math.floor(Math.random() * allCharacter.length));
     console.log(newPassword);
